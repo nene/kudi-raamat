794573606ccd1b5207a519256af2484637b7a5df
Rene Saarsoo <nene@triin.net>
2014-11-25 22:19:13 +0200

    Turn CSS into SCSS file.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+/* global require */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    return gulp.src('raamat.scss')
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('*.scss', ['sass']);
+});
