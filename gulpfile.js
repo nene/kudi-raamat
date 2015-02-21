c5a3e9f1452cba62cdc17591fe17b9f218a9c183
Rene Saarsoo <nene@triin.net>
2015-02-21 12:49:39 +0200

    Add cover file.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('raamat.scss')
+    return gulp.src('*.scss')
         .pipe(sass({
             errLogToConsole: true
         }))
